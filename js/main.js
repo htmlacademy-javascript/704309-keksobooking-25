e4de2713e5bb6501d0da39105efc6795ddac0bb8
Yuri Lopuhin <FOL2203012@yandex.ru>
2022-02-26 03:51:50 +0900

    Внесены изменения в функции получения случайных значений

--- js/main.js
+++ js/main.js
@@ -1,31 +1,34 @@
-'use strict';
 // Функция получения случайного целого числа из переданного диапазона включительно.
 // Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-const getRandomNumber = function (min, max) {
-  if (min <= 0) {
-    return alert('Начальное и конечное значения диапазона должны быть положительными.');
-  }
-
-  if (max <= min) {
-    return alert('Начальное значение должно быть меньше конечного.');
+const getRandomInteger = function (min, max) {
+  if (min < 0 || max < 0) {
+    throw Error('Positive number is required');
+  } else if (max < min) {
+    [min, max] = [max, min];
+  } else if (max === min) {
+    return max;
   }
 
   return Math.round(Math.random() * (max - min) + min);
 };
+//вызов функции, чтобы не было замечаний от ESLint
+getRandomInteger();
 
 // Функция получения случайного числа с плавающей точкой из переданного диапазона включительно.
 
 const getRandomFloat = function (min, max, numbersAfterPoint) {
-  if (min <= 0) {
-    return alert('Начальное и конечное значения диапазона должны быть положительными.');
-  }
-
-  if (max <= min) {
-    return alert('Начальное значение должно быть меньше конечного.');
+  if (min < 0 || max < 0) {
+    throw Error('Positive number is required');
+  } else if (max < min) {
+    [min, max] = [max, min];
+  } else if (max === min) {
+    return max;
   }
 
   const randomNumber = Math.random() * (max - min) + min;
 
   return Number(randomNumber.toFixed(numbersAfterPoint));
 };
+//вызов функции, чтобы не было замечаний от ESLint
+getRandomFloat();
