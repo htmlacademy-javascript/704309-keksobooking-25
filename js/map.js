82090ec2317f933186a5e9ad1fb2f0e9e4d6185c
Yuri Lopukhin <FOL2203012@yandex.ru>
2022-04-09 17:38:30 +0300

    Реализовано отображение 10 объявлений сразу же после загрузки страницы

--- js/map.js
+++ js/map.js
@@ -116,97 +116,96 @@ const closePopup = () => {
 };
 
 //реализация фильтрации всех select и checkbox
+const defaultValue = 'any';
+const housingTypeElement = document.querySelector('#housing-type');
+const housingPriceElement = document.querySelector('#housing-price');
+const housingRoomsElement = document.querySelector('#housing-rooms');
+const housingGuestsElement = document.querySelector('#housing-guests');
+housingPriceElement.value = housingTypeElement.value = housingRoomsElement.value = housingGuestsElement.value = defaultValue;
 
+//функция для проверки селекта "housing-type"
+const checkType = (rowDataType) => {
+  if (housingTypeElement.value === rowDataType.offer.type || housingTypeElement.value === defaultValue) {
+    return true;
+  }
+};
+
+//функция для проверки селекта "housing-price"
+const checkPrice = (rowDataPrice) => {
+  if ((housingPriceElement.value === defaultValue) ||
+    (housingPriceElement.value === 'low' && rowDataPrice.offer.price <= 10000) ||
+    (housingPriceElement.value === 'middle' && rowDataPrice.offer.price > 10000 && rowDataPrice.offer.price <= 50000) ||
+    (housingPriceElement.value === 'high' && rowDataPrice.offer.price > 50000)) {
+    return true;
+  }
+};
+
+//функция для проверки селекта "housing-rooms"
+const checkRooms = (rowDataRooms) => {
+  if (housingRoomsElement.value === defaultValue ||
+    (housingRoomsElement.value === '1' && housingRoomsElement.value === String(rowDataRooms.offer.rooms)) ||
+    (housingRoomsElement.value === '2' && housingRoomsElement.value === String(rowDataRooms.offer.rooms)) ||
+    (housingRoomsElement.value === '3' && housingRoomsElement.value === String(rowDataRooms.offer.rooms)) ||
+    (housingRoomsElement.value === '100' && housingRoomsElement.value === String(rowDataRooms.offer.rooms))) {
+    return true;
+  }
+};
+
+//функция для проверки селекта "housing-guests"
+const checkGuests = (rowDataGuests) => {
+  if (housingGuestsElement.value === defaultValue ||
+    (housingGuestsElement.value === '3' && housingGuestsElement.value === String(rowDataGuests.offer.guests)) ||
+    (housingGuestsElement.value === '2' && housingGuestsElement.value === String(rowDataGuests.offer.guests)) ||
+    (housingGuestsElement.value === '1' && housingGuestsElement.value === String(rowDataGuests.offer.guests)) ||
+    (housingGuestsElement.value === '0' && housingGuestsElement.value === String(rowDataGuests.offer.guests))) {
+    return true;
+  }
+};
+
+//функция для проверки чекбоксов "housing-features"
+const defaultCheckedValue = false;
+const checkboxFilterWifiElement = document.querySelector('#filter-wifi');
+const checkboxFilterDishwasherElement = document.querySelector('#filter-dishwasher');
+const checkboxFilteParkingElement = document.querySelector('#filter-parking');
+const checkboxFilterWasherElement = document.querySelector('#filter-washer');
+const checkboxFilterElevatorElement = document.querySelector('#filter-elevator');
+const checkboxFilterConditionerElement = document.querySelector('#filter-conditioner');
+const arrayFilterElements = [checkboxFilterWifiElement, checkboxFilterDishwasherElement, checkboxFilteParkingElement, checkboxFilterWasherElement, checkboxFilterElevatorElement, checkboxFilterConditionerElement];
+function checkFeatures (rowDataFeatures) {
+  if (rowDataFeatures.offer.features === undefined) {
+    // избавляемся от ошибки в те моменты, когда у предложения вообще отсутствуют преимущества (features)
+    rowDataFeatures.offer.features = [];
+  }
+  if ((checkboxFilterWifiElement.checked || checkboxFilterDishwasherElement.checked || checkboxFilteParkingElement.checked ||
+    checkboxFilterWasherElement.checked || checkboxFilterElevatorElement.checked || checkboxFilterConditionerElement.checked) === defaultCheckedValue) {
+    return true;
+  }
+  const arrayCheckedFilterElements = [];
+  for (let i = 0; i < arrayFilterElements.length; i++) {
+    if (arrayFilterElements[i].checked === true) {
+      arrayCheckedFilterElements.push(arrayFilterElements[i].value);
+    }
+  }
+  const arrayAmountEqualElements = [];
+  for (let i = 0; i < arrayCheckedFilterElements.length; i++) {
+    if (rowDataFeatures.offer.features.includes(arrayCheckedFilterElements[i])) {
+      arrayAmountEqualElements.push(arrayCheckedFilterElements[i]);
+    }
+  }
+  if (arrayAmountEqualElements.length === arrayCheckedFilterElements.length) {
+    return true;
+  }
+}
 function getFilteredData (rowData) {
+  const MAX_ADS_AMOUNT = 10;
+  renderSimpleMarkers(rowData.slice(0,MAX_ADS_AMOUNT)); //получение и отрисовка первых 10 объявлений при загрузке страницы
   const mapForm = document.querySelector('.map__filters');
-  const defaultValue = 'any';
-  const housingTypeElement = document.querySelector('#housing-type');
-  const housingPriceElement = document.querySelector('#housing-price');
-  const housingRoomsElement = document.querySelector('#housing-rooms');
-  const housingGuestsElement = document.querySelector('#housing-guests');
-  housingPriceElement.value = housingTypeElement.value = housingRoomsElement.value = housingGuestsElement.value = defaultValue;
-
   mapForm.addEventListener('change', (evt) => {
     closePopup();                 //закрытие открытого попапа
     resetMarkersAndMapCoords();   //возвращение карты к начальным координатам
-    //функция для проверки селекта "housing-type"
-    const checkType = (rowDataType) => {
-      if (housingTypeElement.value === rowDataType.offer.type || housingTypeElement.value === defaultValue) {
-        return true;
-      }
-    };
-
-    //функция для проверки селекта "housing-price"
-    const checkPrice = (rowDataPrice) => {
-      if ((housingPriceElement.value === defaultValue) ||
-        (housingPriceElement.value === 'low' && rowDataPrice.offer.price <= 10000) ||
-        (housingPriceElement.value === 'middle' && rowDataPrice.offer.price > 10000 && rowDataPrice.offer.price <= 50000) ||
-        (housingPriceElement.value === 'high' && rowDataPrice.offer.price > 50000)) {
-        return true;
-      }
-    };
-
-    //функция для проверки селекта "housing-rooms"
-    const checkRooms = (rowDataRooms) => {
-      if (housingRoomsElement.value === defaultValue ||
-        (housingRoomsElement.value === '1' && housingRoomsElement.value === String(rowDataRooms.offer.rooms)) ||
-        (housingRoomsElement.value === '2' && housingRoomsElement.value === String(rowDataRooms.offer.rooms)) ||
-        (housingRoomsElement.value === '3' && housingRoomsElement.value === String(rowDataRooms.offer.rooms)) ||
-        (housingRoomsElement.value === '100' && housingRoomsElement.value === String(rowDataRooms.offer.rooms))) {
-        return true;
-      }
-    };
-
-    //функция для проверки селекта "housing-guests"
-    const checkGuests = (rowDataGuests) => {
-      if (housingGuestsElement.value === defaultValue ||
-        (housingGuestsElement.value === '3' && housingGuestsElement.value === String(rowDataGuests.offer.guests)) ||
-        (housingGuestsElement.value === '2' && housingGuestsElement.value === String(rowDataGuests.offer.guests)) ||
-        (housingGuestsElement.value === '1' && housingGuestsElement.value === String(rowDataGuests.offer.guests)) ||
-        (housingGuestsElement.value === '0' && housingGuestsElement.value === String(rowDataGuests.offer.guests))) {
-        return true;
-      }
-    };
-
-    //функция для проверки чекбоксов "housing-features"
-    const defaultCheckedValue = false;
-    const checkboxFilterWifiElement = document.querySelector('#filter-wifi');
-    const checkboxFilterDishwasherElement = document.querySelector('#filter-dishwasher');
-    const checkboxFilteParkingElement = document.querySelector('#filter-parking');
-    const checkboxFilterWasherElement = document.querySelector('#filter-washer');
-    const checkboxFilterElevatorElement = document.querySelector('#filter-elevator');
-    const checkboxFilterConditionerElement = document.querySelector('#filter-conditioner');
-    const arrayFilterElements = [checkboxFilterWifiElement, checkboxFilterDishwasherElement, checkboxFilteParkingElement, checkboxFilterWasherElement, checkboxFilterElevatorElement, checkboxFilterConditionerElement];
-    function checkFeatures (rowDataFeatures) {
-      if (rowDataFeatures.offer.features === undefined) {
-        // избавляемся от ошибки в те моменты, когда у предложения вообще отсутствуют преимущества (features)
-        rowDataFeatures.offer.features = [];
-      }
-      if ((checkboxFilterWifiElement.checked || checkboxFilterDishwasherElement.checked || checkboxFilteParkingElement.checked ||
-        checkboxFilterWasherElement.checked || checkboxFilterElevatorElement.checked || checkboxFilterConditionerElement.checked) === defaultCheckedValue) {
-        return true;
-      }
-      const arrayCheckedFilterElements = [];
-      for (let i = 0; i < arrayFilterElements.length; i++) {
-        if (arrayFilterElements[i].checked === true) {
-          arrayCheckedFilterElements.push(arrayFilterElements[i].value);
-        }
-      }
-      const arrayAmountEqualElements = [];
-      for (let i = 0; i < arrayCheckedFilterElements.length; i++) {
-        if (rowDataFeatures.offer.features.includes(arrayCheckedFilterElements[i])) {
-          arrayAmountEqualElements.push(arrayCheckedFilterElements[i]);
-        }
-      }
-      if (arrayAmountEqualElements.length === arrayCheckedFilterElements.length) {
-        return true;
-      }
-    }
-
     if (!(evt.target.nodeName === 'SELECT' || evt.target.nodeName === 'INPUT')) {
       return null;
     }
-    const MAX_ADS_AMOUNT = 10;
     const resultArray = [];
     for (let i = 0; i < rowData.length; i++) {
       if (checkType(rowData[i]) && checkPrice(rowData[i]) && checkRooms(rowData[i]) && checkGuests(rowData[i]) && checkFeatures(rowData[i])) {
